3d66080307d7adce2da109708e2fe92cb1063100
Knut Kirkhorn <knutkirk@hotmail.com>
2018-08-28 01:25:19 +0200

    Add more info on the tasks and a computation of the DP matrix of `S` and `R`

--- exercise-1/main.py
+++ exercise-1/main.py
@@ -13,3 +13,13 @@ a = lcs_matrices[0]
 b = lcs_matrices[1]
 
 LCS.print_tables(a, b, s, r)
+
+# TODO:
+# Task 1 - Compute the LCS DP matrix for LCS(ATTCGGTTA, TAGTGATG)
+s = "ATTCGGTTA"
+r = "TAGTGATG"
+matrix = LCS.compute_dp_matrix(s, r)
+
+# Task 2 - Find the LCS without using a backtrack matrix
+
+# Task 3 - Find all LCSs for a S and R
